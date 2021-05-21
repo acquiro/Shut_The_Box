3443c67ae0b40144aa0f15fee840e89d76672397
acquiro <47385387+acquiro@users.noreply.github.com>
2021-05-21 14:56:16 +0200

    Update Shut_The_Boxe_V3.ps1

--- Shut_The_Boxe_V3.ps1
+++ Shut_The_Boxe_V3.ps1
@@ -139,12 +139,12 @@ function Get-Combinations {
 #Main menue
 function Start-Shut_The_Boxe {
     param(
-    [Parameter(Mandatory)]
+    [Parameter()]
         [ValidateSet(2,3,4,5,6)]
-        [int]$number_of_players,
+        [int]$number_of_players = 2,
 
     [Parameter()]
-        [ValidateSet(9,10,11,12)]
+        [ValidateSet(9,10,11,12,13,14)]
         [int]$table_size = 9
     )
 
@@ -160,15 +160,21 @@ function Scoreboard ($number_of_players){
 
 }
 
+$throw_logs = @() 
+$throw_log = [pscustomobject]@{Throw = "" ; Selection = @() ; Player = ""} 
+$round_logs = [pscustomobject]@{ Round = "" ;  Logs = $throw_logs }
+
+$throw_logs
+
 function Start-Play_Round{
     Param
     (
         [Parameter(Mandatory)]
         [int]$table_size
     )
+    Write-Host "New round"
 
     for ($i = 0 ; $i -lt ($global:scoreboard.count) ; $i++){
-    
         $dice_color = Read-Host "$($scoreboard[$i].Player) dice collors (Default white) Black, DarkBlue, DarkGreen, DarkCyan, DarkRed, DarkMagenta, DarkYellow, Gray, DarkGray, Blue, Green, Cyan, Red, Magenta, Yellow, White"
         clear
         new_play_table -board_size $table_size
@@ -177,12 +183,17 @@ function Start-Play_Round{
         
                 $playground
 
-                if ((Get-Variable tile_*)  | Where-Object { ($_.Name -Match "[1][0-2]")}) { 
+                if ((Get-Variable tile_*)  | Where-Object { ($_.Name -Match "[1][0-4]")}) { 
                     $number_of_dice = 0
                     while (($number_of_dice -ne 2) -and ($number_of_dice -ne 3)){ 
                         $number_of_dice = Read-Host "Tiles above 9 are left, roll [2] or [3] dices "
                     }
-                    $result_throw = Get-AsciiDice -random $number_of_dice -DieColor $dice_color
+                    if ($dice_color -eq $null){
+                        $result_throw = Get-AsciiDice -random $number_of_dice 
+                    }
+                    else{
+                        $result_throw = Get-AsciiDice -random $number_of_dice -DieColor $dice_color
+                    }
                 }
                 elseif  ((Get-Variable tile_*)  | Where-Object {($_.Name -Match "[6-9]")}) {
                       $result_throw = Get-AsciiDice -Random 2 -DieColor $dice_color                
@@ -202,7 +213,7 @@ function Start-Play_Round{
                 $result_throw.Dice | % {Write-Host $_ -ForegroundColor $($result_throw.Color)}
 
                 if ($possibilities -eq $null){
-                    Write-Host "No possible combination left"
+                    Write-Host "No possible combination left" -ForegroundColor White
                     Write-Host "End of $($Global:scoreboard[$i].Player) round"
                     $player_score = 0
                     $available_tiles | % { $player_score += $_}
@@ -259,6 +270,12 @@ function Start-Play_Round{
                         Write-Host "Shut the boxe !!! You win this round!"
                         $global:scoreboard[$i].Round_Won += 1
                         $global:scoreboard[0..($global:scoreboard.count)] | % {$_.Score = 0 }
+                        $new_round = Read-Host "Another round ?  [Y]es / [N]o"
+                        if (($new_round -eq 'Y') -or ($new_round -eq 'yes')){ 
+                            clear
+                            Start-Play_Round -table_size $table_size
+                        }
+                        else {break}
                     }
                 }
             }   
@@ -268,7 +285,7 @@ function Start-Play_Round{
     $round_winner =  $scoreboard | Sort-Object -Property Current_Score | select -First 1 
     Write-Host "$($round_winner.Player) win this round with $($round_winner.Current_Score) points"   
     $scoreboard | Sort-Object -Property Current_Score | select -First 1 | % {$_.Round_Won += 1}
-    $scoreboard
+    echo $scoreboard
     $scoreboard | % {$_.Current_Score = 0}   
     $new_round = Read-Host "Another round ?  [Y]es / [N]o"
     if (($new_round -eq 'Y') -or ($new_round -eq 'yes')){ 
